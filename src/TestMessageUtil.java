ca00c02c1f87a65b09332ee0cddf127ba97de8f7
thuthulinn <thuthulinn11@gmail.com>
2017-07-18 13:41:08 +0630

    Update TestMessageUtil.java

--- src/TestMessageUtil.java
+++ src/TestMessageUtil.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class TestMessageUtil {
 
-	   String message = "Hello!,Robert";	
+	   String message = "Robert";	
 	   MessageUtil messageUtil = new MessageUtil(message);
 	   
 	   @Test
