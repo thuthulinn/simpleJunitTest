244f51e190ee147a9df7828fc57ec37fa4e87fc9
thuthulinn <thuthulinn11@gmail.com>
2017-07-18 13:18:18 +0630

    Add files via upload

---
+++ src/TestMessageUtil.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestMessageUtil {
+
+	   String message = "Hello!,Robert";	
+	   MessageUtil messageUtil = new MessageUtil(message);
+	   
+	   @Test
+	   public void testPrintMessage() {	
+	      System.out.println("Inside testPrintMessage()");     
+	      assertEquals(message,messageUtil.printMessage());
+	   }
+
+	   @Test
+	   public void testSalutationMessage() {
+	      System.out.println("Inside testSalutationMessage()");
+	      message = "Hi!" + "Hello!,Robert";
+	      assertEquals(message,messageUtil.salutationMessage());
+	   }
+}
